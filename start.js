babb9583fff758cecdeb56b064ebf1c3a602ed14
TsubasaKondo <Tsubasa@FujiProvingGrounds.com>
2015-09-22 10:20:40 +0900

    Unnecessary commented-out code removed from start.js

--- start.js
+++ start.js
@@ -1,29 +1,16 @@
 $(document).ready(function(){
+  
   $('button').on('mouseenter',function(){
     $(this).animate({'padding':'10 15 10 15' });
   });
+
   $('button').on('mouseleave',function(){
     $(this).animate({'padding':'5 10 5 10' });
   });
-  // $('button').on('click',function(){
-  //   $(this).fadeOut();
-  // });
 
   $('button #startAnAar').on('click',function(){
     $(this).fadeOut();
     location.href='newaar.html';
   });
-
-  // $('input[type=button]').on('mouseenter',function(){
-  //   $(this).animate({'padding':'10 15 10 15' });
-  // });
-  //
-  // $('input[type=button]').on('mouseleave',function(){
-  //   $(this).animate({'padding':'5 10 5 10' });
-  // });
-  //
-  // $('input[type=button]').on('click',function(){
-  //   $(this).fadeOut();
-  // });
 
 });
